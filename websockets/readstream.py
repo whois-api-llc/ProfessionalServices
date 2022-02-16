d6e97f52b0d5542e1fb51b7a6829ad278315fc2a
Ed Gibbs <92936884+wxacoeur@users.noreply.github.com>
2022-02-16 10:49:36 -0500

    Create readstream.py

---
+++ websockets/readstream.py
@@ -0,0 +1,33 @@
+# Simple Websocket client to connect and continiously read wss: service after sending apiKey
+# Be sure to specify your API key
+# Requirements: pip3 install websocket-client
+# Specifications can be found at: https://domain-registration-streaming.whoisxmlapi.com/specifications/data-streaming
+# Sample provided by WHOISXMLAPI.COM, credit goes to https://pypi.org/project/websocket-client/
+
+from websocket import create_connection
+import sys, signal
+import time
+
+ws = create_connection("wss://nrd-stream.whoisxmlapi.com/ultimate")
+
+apiKey = "at_xZK2rardlymYkflUSs99vqsEyKoaj"
+
+print("Sending API Key...")
+ws.send(apiKey)
+print("API Key Sent")
+
+print("Receiving WHOIS data (press ctrl/c to terminate)...")
+
+time.sleep(3)
+
+while True:
+    try:
+        result =  ws.recv()
+        print("Record Received: '%s'" % result)
+    except:
+        print("Exiting.")
+        break
+
+ws.close()
+
+print("Websocket closed.")
