f6d5d0f06d57cb7a7626f0dc4afbf685ceed0de7
WXA-ProfessionalServices <92936884+wxacoeur@users.noreply.github.com>
2024-10-11 11:00:03 -0400

    Update ipnetblocks_download.sh

--- scripts/ipnetblocks/ipnetblocks_download.sh
+++ scripts/ipnetblocks/ipnetblocks_download.sh
@@ -6,6 +6,7 @@ nooutput="--quiet"
 # nooutput=""
 ipblock_logfile="ipnetblocks-download.log"
 today=$(date '+%Y-%m-%d')
+rundate=$(date)  # Capture the full timestamp for logging
 savedir="./"
 auth="at_YOURAPIKEY"
 ipnetblockURL="https://ip-netblocks.whoisxmlapi.com/datafeeds/IP_Netblocks_WHOIS_Database/"
@@ -13,12 +14,16 @@ ip_file="ip_netblocks.$today.full.blocks.csv.gz"
 ip_fullURL="$ipnetblockURL$ip_file"
 fetcher="wget"
 cmd="$fetcher $nooutput --user $auth --password $auth $ip_fullURL -O $savedir$ip_file"
-# echo "[ $rundate ] Begin download for $today: $cmd"
+
+# Logging the start of the download
 echo "[ $rundate ] Begin download for $today: $cmd" >> $ipblock_logfile
+
+# Executing the download command
 wresult=$($cmd)
-if [ $? -ne 0 ];
-  then
-     echo "File not found $savedir$ip_filetype" >> $ipblock_logfile
-  else
-     echo "Download successful $ip_file" >> $ipblock_logfile
+
+# Error handling and logging
+if [ $? -ne 0 ]; then
+    echo "[ $rundate ] File not found or download failed: $savedir$ip_file" >> $ipblock_logfile
+else
+    echo "[ $rundate ] Download successful: $savedir$ip_file" >> $ipblock_logfile
 fi
